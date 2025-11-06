e6c0b4630e4ef37f4ccec3b83a45324995eb6303
Isa-fee <tiagosoaresjp1995@gmail.com>
2025-11-06 08:28:58 -0300

    tentativa do index

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_mysqldb import MySQL
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 app.secret_key = 'chave_super_segura'  # Necessária para flash messages e sessões
-
 # Configuração do MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -16,8 +15,8 @@ mysql = MySQL(app)
 # Importa e registra os blueprints (rotas)
 from controllers.main.routes import main_bp
 from controllers.auth.routes import auth_bp
-# from controllers.livro.routes import livro_bp
+from controllers.livro.routes import livro_bp
 
-# app.register_blueprint(main_bp)
+app.register_blueprint(main_bp)
 app.register_blueprint(auth_bp, url_prefix='/auth')
-# app.register_blueprint(livro_bp, url_prefix='/livros')
+app.register_blueprint(livro_bp, url_prefix='/livros')
