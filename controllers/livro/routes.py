8952c87289263575d3afd5cf3f0d6902c8d68a01
medeirostamiris <tamirismdrs99@gmail.com>
2025-11-06 17:36:32 -0300

    criando templates e adicionando crud

--- controllers/livro/routes.py
+++ controllers/livro/routes.py
@@ -26,3 +26,35 @@ def add_livro():
         flash('Livro adicionado com sucesso!', 'success')
         return redirect(url_for('livro_bp.view_livros'))
     return render_template('add_livro.html')
+
+@livro_bp.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_livro(id):
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM livros WHERE id=%s', (id,))
+    livro = cursor.fetchone()
+
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        autor = request.form['autor']
+        ano = request.form['ano']
+
+        cursor.execute('UPDATE livros SET titulo=%s, autor=%s, ano=%s WHERE id=%s', (titulo, autor, ano, id))
+        mysql.connection.commit()
+        cursor.close()
+
+        flash('Livro atualizado com sucesso!', 'success')
+        return redirect(url_for('livro_bp.view_livros'))
+
+    cursor.close()
+    return render_template('edit_livro.html', livro=livro)
+
+
+@livro_bp.route('/delete/<int:id>')
+def delete_livro(id):
+    cursor = mysql.connection.cursor()
+    cursor.execute('DELETE FROM livros WHERE id=%s', (id,))
+    mysql.connection.commit()
+    cursor.close()
+    flash('Livro removido com sucesso!', 'info')
+    return redirect(url_for('livro_bp.view_livros'))
+
