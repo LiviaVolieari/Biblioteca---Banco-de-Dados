daae47fe9c3c236dc26915a1e8043fd7198ba58b
Isa-fee <tiagosoaresjp1995@gmail.com>
2025-11-16 21:35:00 -0300

    ajeitando erro no css

--- config/__init__.py
+++ config/__init__.py
@@ -8,7 +8,8 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'style')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 
 
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
