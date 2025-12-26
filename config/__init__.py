81ed024d9cac0057a5d8735f27b7f2759c5267ea
Lívia Tainá <liviataina009@gmail.com>
2025-12-26 10:46:08 -0300

    Primeiro trigger de Geração Automática de Valores feito, trg_log_devolucao.

--- config/__init__.py
+++ config/__init__.py
@@ -27,7 +27,7 @@ login_manager.login_message_category = 'warning'
 app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', 'localhost')
 app.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT', 3306))
 app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'root')
-app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', '')  # a senha é definida aqui
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', '1234')  # a senha é definida aqui
 app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'biblioteca')
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
